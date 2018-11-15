afb03f6bd9bbd2a31efce17635aa3433320d1057
th3eesome <610870693@qq.com>
2018-11-15 15:10:41 +0800

    json viewer available now

--- src/app/generalComponents/general-components-list/general-components-list.component.ts
+++ src/app/generalComponents/general-components-list/general-components-list.component.ts
@@ -4,76 +4,27 @@ import {NestedTreeControl} from '@angular/cdk/tree';
 import { MatTreeNestedDataSource } from '@angular/material';
 import { BehaviorSubject } from 'rxjs';
 
-export class JsonNode {
-  children: JsonNode[];
-  name: string;
-  type: any;
-}
-
-
-const TREE_DATA = JSON.stringify(components_list[0]);
-
-@Injectable()
-export class NodeDatabase {
-  dataChange = new BehaviorSubject<JsonNode[]>([]);
-  get data(): JsonNode[] {return this.dataChange.value; }
-
-  constructor() {
-    this.initialize();
-  }
-
-  initialize() {
-    const dataObject = JSON.parse(TREE_DATA);
-    const data = this.buildJsonTree(dataObject, 0);
-    this.dataChange.next(data);
-  }
-
-  buildJsonTree(obj: {[key: string]: any}, level: number): JsonNode[] {
-    return Object.keys(obj).reduce<JsonNode[]>((accumulator, key) => {
-        const value = obj[key];
-        const node = new JsonNode();
-        node.name = key;
-        if (value != null) {
-            if (typeof value === 'object') {
-                node.children = this.buildJsonTree(value, level + 1);
-            } else {
-                node.type = value;
-            }
-        }
-        return accumulator.concat(node);
-    }, []);
-  }
-}
 
 @Component({
   selector: 'app-general-components-list',
   templateUrl: './general-components-list.component.html',
   styleUrls: ['./general-components-list.component.css'],
-  providers: [NodeDatabase]
 })
 export class GeneralComponentsListComponent implements OnInit {
 
-  nestedTreeControl: NestedTreeControl<JsonNode>;
-  nestedDataSource: MatTreeNestedDataSource<JsonNode>;
   components_list = components_list;
   show_source = false;
-  source_code = JSON.stringify(this.components_list[0]);
+  source_code = this.components_list[0];
 
-  constructor(database: NodeDatabase) {
-    this.nestedTreeControl = new NestedTreeControl<JsonNode>(this._getChildren);
-    this.nestedDataSource = new MatTreeNestedDataSource();
-    database.dataChange.subscribe(data => this.nestedDataSource.data = data);
+  constructor() {
   }
-
-  hasNestedChild = (_: number, nodeData: JsonNode) => !nodeData.type;
-
-  private _getChildren = (node: JsonNode) => node.children;
 
   ngOnInit() {
   }
 
   changeTab(e) {
-    this.source_code = JSON.stringify(this.components_list[e.index]);
+    this.source_code = this.components_list[e.index];
+    this.show_source = false;
   }
 
   showSource() {
