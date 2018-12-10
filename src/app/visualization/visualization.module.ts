a5794b963e918359686006a163409618987d0b7b
HelloCrease <Lian123@stu.xjtu.edu.cn>
2018-12-10 21:41:09 +0800

    tree

--- src/app/visualization/visualization.module.ts
+++ src/app/visualization/visualization.module.ts
@@ -5,12 +5,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DemoMaterialModule } from '@app/demo-material-module';
 import { HttpClientModule } from '@angular/common/http';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { TreeModule } from 'angular-tree-component';
 
 import { ViewPaneComponent } from './view-pane/view-pane.component';
 import { PlotPaneComponent } from './plot-pane/plot-pane.component';
 import { DataPaneComponent } from './data-pane/data-pane.component';
 import { MainComponent } from './main/main.component';
 import { DimensionReductionComponent } from './dimension-reduction/dimension-reduction.component';
+import { TreeComponent } from './tree/tree.component';
 
 export const visRoutes: Routes = [
   { path: '', component: MainComponent}
@@ -23,8 +25,16 @@ export const visRoutes: Routes = [
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    DragDropModule
+    DragDropModule,
+    TreeModule.forRoot()
   ],
-  declarations: [ViewPaneComponent, PlotPaneComponent, DataPaneComponent, MainComponent, DimensionReductionComponent]
+  declarations: [
+    ViewPaneComponent,
+    PlotPaneComponent,
+    DataPaneComponent,
+    MainComponent,
+    DimensionReductionComponent,
+    TreeComponent
+]
 })
 export class VisualizationModule { }
