f3b200226530133323e08af3579231e21e92f065
Xujingqin <845150220@qq.com>
2018-12-11 10:32:10 +0800

    markdown

--- src/app/mammary/mammary-form/mammary-form.component.ts
+++ src/app/mammary/mammary-form/mammary-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { survey_list } from '../surveyList';
 import { HttpService } from '@app/core/services/http.service';
+import { ActivatedRoute } from '@angular/router';
 import {init} from 'protractor/built/launcher';
 
 @Component({
@@ -12,11 +13,22 @@ export class MammaryFormComponent implements OnInit {
 
   mammary_list = survey_list;
 
-  constructor(private http: HttpService) { }
+  constructor(
+    private http: HttpService,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
     this.initForm().subscribe((res) => {
       console.log(res);
+      // fill in the answer right here
+      // const part1 = this.mammary_list[0].items[0]['layout'][1];
+ 
+      // if (part1.hasOwnProperty("key_value")) {
+      //   console.log(part1.key_value._key);
+      //   console.log(res.data);
+      //   part1.key_value._value = res.data[part1.key_value._key];
+      // }
       for ( let i = 0; i < this.mammary_list[0].items[0]['layout'].length; i++) {
         const part1 = this.mammary_list[0].items[0]['layout'][i];
         if (part1.key_value) {
