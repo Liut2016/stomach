e5c5495bed6474dca5023a2f321f6795c53d53df
th3eesome <610870693@qq.com>
2018-12-21 20:29:54 +0800

    little change

--- src/app/stomach/stomach-form/stomach-form.component.ts
+++ src/app/stomach/stomach-form/stomach-form.component.ts
@@ -25,7 +25,6 @@ export class StomachFormComponent implements OnInit {
       console.log(res);
       // fill in the answer right here
       // const part1 = this.mammary_list[0].items[0]['layout'][1];
- 
       // if (part1.hasOwnProperty("key_value")) {
       //   console.log(part1.key_value._key);
       //   console.log(res.data);
